c75793dcb2753d42f98025d4b3a968bf133ba3f9
jhenchem0210 <jhenchem0210@gmail.com>
2023-01-29 16:23:29 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/Login.jsx 	new file:   src/Register.jsx 	deleted:    src/components/Footer.js 	deleted:    src/components/Header.js 	deleted:    src/components/LOGOBLACK.png 	deleted:    src/components/man.png 	modified:   src/index.js 	deleted:    src/index2.js 	deleted:    src/index3.js 	deleted:    src/indexhandlesubmit.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
+import React, { useState } from "react";
 import './App.css';
-import logo from './logogopil.png';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { Login } from "./Login";
+import { Register } from "./Register";
 
 function App() {
-  const firstName = 'Juan';
-  const lastName = 'Dela Cruz';
-  const age = '25';
-  
-  const arr =[1,2,3,4,5]
+  const [currentForm, setCurrentForm] = useState('login');
 
-
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
 
   return (
-    <>
-    <Header />
-    <main>
-    <h1>Hello World! {firstName} {lastName} {age}</h1>
-    <h2>Multiples of 5</h2>
-    {arr.map((num => <p> {num * 5}</p>))}
-    <img src={logo} className="App-logo" alt="logo" />
-    <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I want to explore and Learn more of REACT!
-        </a>
-    </main>
-    <Footer />
-    </>
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
+    </div>
   );
- }
+}
 
- export default App;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,45 +1,12 @@
-import { useState } from "react";
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-function MyForm() {
-  const [inputs, setInputs] = useState({});
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs (values => ({...values, [name]: value}))
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault ();
-    console.log(inputs);
-
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>Enter your name:
-        <input 
-          type="text" 
-          name= "username"
-          value = {inputs.username || ""}
-          onChange= {handleChange}
-        />
-      </label>
-
-      <label>Enter your AGE:
-        <input 
-          type="number" 
-          name= "age"
-          value = {inputs.age || ""}
-          onChange= {handleChange}
-        />
-      </label>
-
-      <input type="submit" />
-    </form>
-  )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MyForm />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
